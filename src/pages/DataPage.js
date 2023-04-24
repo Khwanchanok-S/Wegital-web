2bbfd60f5b3550ca77ff60b85a9f26d55d85f7a2
khwanchanok <khwanchanoksriphattharasith@gmail.com>
2023-04-24 16:48:05 +0700

    add button logout

--- src/pages/DataPage.js
+++ src/pages/DataPage.js
@@ -66,7 +66,17 @@ export default function DataPage() {
                   </button>
                 </Link>
               </div>
-              <div className=""></div>
+              <div className="">
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={logout}
+                  >
+                    ออกจากระบบ
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
